ef0642fd17f2ea15abd39444d11aa51b26cdb341
Alicja Dabrowska <alicja.dab27@gmail.com>
2023-03-24 21:30:40 +0100

    Add Shopping Cart Features and Redux

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,27 +1,36 @@
-import React, {useState, useEffect} from 'react'
-import { Link, useParams } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap"
+import React, { useState, useEffect } from 'react'
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
+import { Row, Col, Image, ListGroup, Button, Card, Form } from "react-bootstrap"
+import { listProductDetails } from "../actions/productActions"
 import Rating from '../components/Rating';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 
-import axios from 'axios';
+const ProductScreen = ({history}) => {
+  const [qty, setQty] = useState(1);
 
-const ProductScreen = () => {
-  const[product, setProduct] = useState({});
   const product_id = useParams().id;
+  let navigate = useNavigate();
+
+  const dispatch = useDispatch();
+  const productDetails = useSelector(state => state.productDetails);
+  const { error, loading, product } = productDetails;
 
   useEffect(() => {
-    const fetchProduct = async () => {
-      // dodać do package.json ->  "proxy": "http://127.0.0.1:8000"
-      const {data} = await axios.get(`/api/products/${product_id}`)
-      setProduct(data);
-    }
+    dispatch(listProductDetails(product_id));
+    }, [dispatch ,product_id]);
 
-    fetchProduct();
-  }, [])
-  // const product = products.find(p => p._id === product_id.id)
+  const addToCartHandler = () => {
+    navigate(`/cart/${product_id}?qty=${qty}`);
+  };
+
   return (
     <div>
       <Link to='/' className='btn btn-secondary my-3'>Go Back</Link>
+      { loading ? <Loader />
+      : error ? <Message variant='danger'>{error}</Message>
+      : 
       <Row>
         <Col md={6}>
           <Image src={product.image} alt={product.name} fluid/>
@@ -41,11 +50,11 @@ const ProductScreen = () => {
             </ListGroup.Item>
 
             <ListGroup.Item>
-               <h6>Price: ${product.price}</h6>
+              <h6>Price: ${product.price}</h6>
             </ListGroup.Item>
 
             <ListGroup.Item>
-               Description: {product.description}
+              Description: {product.description}
             </ListGroup.Item>
 
           </ListGroup>
@@ -71,13 +80,46 @@ const ProductScreen = () => {
                 </Row>
               </ListGroup.Item>
 
+              {product.countInStock > 0 && (
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Quantity:</Col>
+                    <Col className='my-1'>
+                      <Form.Control 
+                        onChange={e => setQty(e.target.value)}
+                        as='select' 
+                        value={qty}
+                      >
+                        {
+                          [...Array(product.countInStock).keys()].map(x => (
+                            <option key={x + 1} value={x+1}>
+                              {/* cause array start 0 */}
+                              {x + 1} 
+                            </option>
+                          )) 
+                        } 
+                          
+                      </Form.Control>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              )}
+
               <ListGroup.Item>
-                <Button className='w-100' disabled={product.countInStock === 0 } type='button'>Add To Cart</Button>
+                <Button 
+                  className='w-100' 
+                  disabled={product.countInStock === 0 } 
+                  type='button'
+                  onClick={addToCartHandler}
+                  >
+                    Add To Cart
+                </Button>
               </ListGroup.Item>
             </ListGroup>
           </Card>
         </Col>
       </Row>
+}
     </div>
   )
 }
